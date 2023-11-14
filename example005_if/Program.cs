4fcb123511e0632ae8e1961798ccb0359e1aebc3
Andrey Savaskov <ultrazokk@gmail.com>
2023-11-14 21:07:43 +0300

    вывод имени

---
+++ example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username == "Nadya")
+{
+    Console.WriteLine("Ура, это же НАДЯ!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
